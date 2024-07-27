020d994dbf713910b82acd8acd20f38057a605a1
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-07-27 11:01:20 +0300

    Install Prettier

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,32 @@
-import { fastify, FastifyInstance, RouteShorthandOptions } from 'fastify'
-import { Server, IncomingMessage, ServerResponse } from 'http'
+import { fastify, FastifyInstance, RouteShorthandOptions } from "fastify";
+import { IncomingMessage, Server, ServerResponse } from "http";
 
-const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({})
+const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
+  fastify({});
 
 const opts: RouteShorthandOptions = {
   schema: {
     response: {
       200: {
-        type: 'object',
+        type: "object",
         properties: {
           pong: {
-            type: 'string'
-          }
-        }
-      }
-    }
-  }
-}
+            type: "string",
+          },
+        },
+      },
+    },
+  },
+};
 
-server.get('/ping', opts, (request, reply) => {
-  reply.code(200).send({ pong: 'it worked and was rebuilt!' })
-})
+server.get("/ping", opts, (request, reply) => {
+  reply.code(200).send({ pong: "pong!" });
+});
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) {
-    console.error(err)
-    process.exit(1)
+    console.error(err);
+    process.exit(1);
   }
-  console.log(`Server listening at ${address}`)
-})
+  console.log(`Server listening at ${address}`);
+});
