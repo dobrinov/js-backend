4412a83ec264aec6854ed0c4d1a8b3b64f9502b2
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-08-03 14:37:30 +0300

    Add CORS support

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 import { fastify, FastifyInstance } from "fastify";
@@ -10,91 +11,119 @@ import { checkPassword } from "./util/password";
 
 const GRAPHIQL = readFileSync("graphiql.html");
 
-const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
-  fastify({ logger: true });
+async function execute() {
+  const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
+    fastify({ logger: true });
 
-server.post<{
-  Body: {
-    query: string;
-    variables?: { [variable: string]: unknown } | null | undefined;
-  };
-}>("/graph", async function (request, response) {
-  const query = request.body.query;
-  const authorization = request.headers.authorization;
+  await server.register(cors, {});
 
-  if (!authorization) return response.code(401).send("Unauthorized");
+  server.post<{
+    Body: {
+      query: string;
+      variables?: { [variable: string]: unknown } | null | undefined;
+    };
+  }>("/graph", async function (request, response) {
+    const query = request.body.query;
+    const authorization = request.headers.authorization;
 
-  const token = authorization.replace("Bearer ", "");
+    if (!authorization)
+      return response.code(401).send(errorResponseBody("Unauthorized"));
 
-  let claims: jose.JWTPayload;
-  try {
-    claims = jose.decodeJwt(token);
-  } catch {
-    return response.code(400).send("Invalid token");
-  }
+    const token = authorization.replace("Bearer ", "");
 
-  if (!claims || !("userId" in claims) || typeof claims.userId !== "number")
-    return response.code(400).send("Invalid token");
+    let claims: jose.JWTPayload;
+    try {
+      claims = jose.decodeJwt(token);
+    } catch {
+      return response.code(400).send(errorResponseBody("Invalid token"));
+    }
 
-  const prisma = new PrismaClient();
-  const currentUser = prisma.user.findUnique({ where: { id: claims.userId } });
+    if (!claims)
+      return response.code(400).send(errorResponseBody("Invalid token"));
 
-  if (!currentUser) return response.code(401).send("Unauthorized");
+    if (!("userId" in claims) || typeof claims.userId !== "number")
+      return response.code(400).send(errorResponseBody("Invalid token"));
 
-  try {
-    const result = await graphql({
-      schema,
-      source: query,
-      contextValue: { currentUser },
-      variableValues: request.body.variables,
-    });
-    return response.send(result);
-  } catch (error) {
-    let message =
-      error instanceof Error ? error.message : "Oops something went wrong";
-    return response.code(500).send(message);
-  }
-});
-
-server.get("/graphiql", async function (_request, response) {
-  response.type("text/html").send(GRAPHIQL);
-});
-
-server.post<{ Body: { email: string; password: string } }>(
-  "/session",
-  async function (request, response) {
-    const alg = "HS256";
-    const secret = process.env.JWT_SECRET;
-
-    const { email, password } = request.body;
     const prisma = new PrismaClient();
+    const currentUser = prisma.user.findUnique({
+      where: { id: claims.userId },
+    });
 
-    const user = await prisma.user.findUnique({ where: { email } });
+    if (!currentUser)
+      return response.code(401).send(errorResponseBody("Unauthorized"));
 
-    if (!user) return response.code(401).send("Invalid email or password");
+    if (!("exp" in claims) || typeof claims.exp !== "number")
+      return response.code(400).send(errorResponseBody("Invalid token"));
 
-    if (!checkPassword(password, user.passwordDigest))
-      return response.code(401).send("Invalid email or password");
+    if (claims.exp * 1000 < Date.now())
+      return response.code(401).send(errorResponseBody("Token expired"));
 
-    if (!secret)
-      return response
-        .code(500)
-        .send("JWT secret is not set in this environment");
+    try {
+      const result = await graphql({
+        schema,
+        source: query,
+        contextValue: { currentUser },
+        variableValues: request.body.variables,
+      });
+      return response.send(result);
+    } catch (error) {
+      let message =
+        error instanceof Error ? error.message : "Oops something went wrong";
+      return response.code(500).send(errorResponseBody(message));
+    }
+  });
 
-    const jwt = await new jose.SignJWT({ userId: user.id })
-      .setProtectedHeader({ alg })
-      .setIssuedAt()
-      .setExpirationTime("2h")
-      .sign(new TextEncoder().encode(secret));
+  server.get("/graphiql", async function (_request, response) {
+    response.type("text/html").send(GRAPHIQL);
+  });
 
-    response.send(jwt);
+  server.post<{ Body: { email: string; password: string } }>(
+    "/session",
+    async function (request, response) {
+      const alg = "HS256";
+      const secret = process.env.JWT_SECRET;
+
+      const { email, password } = request.body;
+      const prisma = new PrismaClient();
+
+      const user = await prisma.user.findUnique({ where: { email } });
+
+      if (!user) return response.code(401).send("Invalid email or password");
+
+      if (!checkPassword(password, user.passwordDigest))
+        return response.code(401).send("Invalid email or password");
+
+      if (!secret)
+        return response
+          .code(500)
+          .send("JWT secret is not set in this environment");
+
+      const jwt = await new jose.SignJWT({ userId: user.id })
+        .setProtectedHeader({ alg })
+        .setIssuedAt()
+        .setExpirationTime("2h")
+        .sign(new TextEncoder().encode(secret));
+
+      response.header("Access-Control-Allow-Origin", "*");
+      response.header("Access-Control-Allow-Methods", "POST");
+      response.send(jwt);
+    }
+  );
+
+  server.listen({ port: 8080 }, (err, address) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Server listening at ${address}`);
+  });
+
+  function errorResponseBody(error: string) {
+    return {
+      errors: [{ message: error }],
+      data: null,
+    };
   }
-);
+}
 
-server.listen({ port: 8080 }, (err, address) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`Server listening at ${address}`);
-});
+execute();
