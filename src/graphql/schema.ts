9c47308611e959338134e0b66ca918e9db290924
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-08-22 10:33:22 +0300

    Add Relay support and users connection

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,9 +1,25 @@
 import {
+  GraphQLEnumType,
   GraphQLNonNull,
   GraphQLObjectType,
   GraphQLSchema,
   GraphQLString,
 } from "graphql";
+import {
+  connectionArgs,
+  connectionDefinitions,
+  cursorToOffset,
+  offsetToCursor,
+} from "graphql-relay";
+import prisma from "../db";
+
+const UserRoleEnumType = new GraphQLEnumType({
+  name: "UserRole",
+  values: {
+    BASIC: { value: "BASIC" },
+    ADMIN: { value: "ADMIN" },
+  },
+});
 
 const user = new GraphQLObjectType({
   name: "User",
@@ -11,7 +27,12 @@ const user = new GraphQLObjectType({
     id: { type: new GraphQLNonNull(GraphQLString) },
     name: { type: new GraphQLNonNull(GraphQLString) },
     email: { type: new GraphQLNonNull(GraphQLString) },
+    role: { type: new GraphQLNonNull(UserRoleEnumType) },
   },
+});
+
+var { connectionType: UserConnection } = connectionDefinitions({
+  nodeType: user,
 });
 
 export const schema = new GraphQLSchema({
@@ -21,6 +42,41 @@ export const schema = new GraphQLSchema({
       viewer: {
         type: new GraphQLNonNull(user),
         resolve: (_parent, _args, contextValue) => contextValue.currentUser,
+      },
+      users: {
+        type: new GraphQLNonNull(UserConnection),
+        args: connectionArgs,
+        resolve: async (_parent, args) => {
+          const usersPlusOne = await prisma.user.findMany({
+            take: args.first + 1,
+            skip: args.after ? cursorToOffset(args.after) : undefined,
+            orderBy: {
+              id: "asc",
+            },
+          });
+
+          const users = usersPlusOne.slice(0, args.first);
+          const hasNextPage = usersPlusOne.length > args.first;
+          const startCursor =
+            users.length > 0 ? offsetToCursor(users[0].id) : null;
+          const endCursor =
+            users.length > 0
+              ? offsetToCursor(users[users.length - 1].id)
+              : null;
+
+          return {
+            edges: users.map((user) => ({
+              cursor: offsetToCursor(user.id),
+              node: user,
+            })),
+            pageInfo: {
+              startCursor,
+              endCursor,
+              hasPreviousPage: false,
+              hasNextPage,
+            },
+          };
+        },
       },
     },
   }),
