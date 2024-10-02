f3638a7fd3c38bddaa7a0f1e4c117360461431ff
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2024-10-02 07:53:21 +0300

    Log DB queries

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient();
+  return new PrismaClient({ log: ["query", "info", "warn", "error"] });
 };
 
 declare const globalThis: {
